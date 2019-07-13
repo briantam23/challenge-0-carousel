f5b7610f7037af1c1b4dbf2bb1d4838c24394f3d
briantam23 <briantam23@gmail.com>
2019-07-13 15:24:33 -0400

    Added self-invoking function

---
+++ scripts/app.js
@@ -0,0 +1,13 @@
+(() => {
+    let count = 1;
+
+    setInterval(() => {
+        const img = document.getElementsByClassName('image')[0];
+        const hero = "hero-image-" + count;
+        
+        img.style.backgroundImage = `url(./images/${hero}.jpg)`;
+
+        count++;
+        if(count === 4) count = 0;
+    }, 3000)
+})()
